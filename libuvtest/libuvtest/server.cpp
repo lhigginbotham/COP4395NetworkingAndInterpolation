8800a614ed9c148f37a1db7302c5329bea91efd5
lhigginbotham <lucas.higginbotham@knights.ucf.edu>
2017-04-11 22:26:24 -0400

    Fix new FrameBuffer allocation on all future packets
    A new FrameBuffer is now only allocated if a FrameBuffer wasn't availiable for the future packet.

--- libuvtest/libuvtest/server.cpp
+++ libuvtest/libuvtest/server.cpp
@@ -18,7 +18,7 @@ const ConfigStore globalConfig ("config1.log");
 
 void listen(uvw::Loop &loop, std::map<std::string, int> &ips) {
 	std::shared_ptr<uvw::UDPHandle> udp = loop.resource<uvw::UDPHandle>();
-	udp->bind("127.0.0.1", 4951);
+	udp->bind("0.0.0.0", 4951);
 	udp->recv();
 	int cPosition = 0;
 
@@ -35,76 +35,93 @@ void listen(uvw::Loop &loop, std::map<std::string, int> &ips) {
 		//Converting it to a Cstring causes Intellisense to no longer flag it
 		//Unsure what a proper fix to this would be as library dev blames it on Intellisense (and the fact that it compiles and runs regardless supports that)
 		nlohmann::json freq = nlohmann::json::parse(complete.c_str());
-		//std::chrono::time_point<std::chrono::system_clock> currentTime = std::chrono::system_clock::now();
 
+		//Check if the framebuffer data struct is empty
 		if (frameBuffer.empty())
 		{
 			time_t recievedTime = freq.value("time", 0);
 			frameBuffer.push_back(FrameBuffer(std::chrono::system_clock::from_time_t(recievedTime)));
 		}
-
+		//Check if incoming ip has been recieved before
 		if (ips.find(sData.sender.ip) == ips.end())
 		{
 			ips.emplace(sData.sender.ip, ips.size());
 			frameBuffer.begin()->sensorBuffer.push_back(std::vector<nlohmann::json>());
 		}
+		//If first use of sensor buffer, resize if to expected size
 		if (frameBuffer[cPosition].sensorBuffer.size() != ips.size())
 		{
 			frameBuffer[cPosition].sensorBuffer.resize(ips.size());//This may break existing iterators so watch out for that
 		}
-
+		//Handle placements of incoming packets based on their time and the current frames time
 		int ipPosition = ips.find(sData.sender.ip)->second;
 		auto front = frameBuffer.begin();
 		std::chrono::milliseconds duration(500);
 		std::chrono::milliseconds freqTime = time_tToMilli(freq.value("time", 0));
 		std::chrono::milliseconds lowerBound = pointToMilli(front->recievedTime);
 		std::chrono::milliseconds upperBound = lowerBound + duration;
+		//Discard
 		if (freqTime < lowerBound)
 		{
 			return;
 		}
+		//Place in current frame
 		else if (freqTime >= lowerBound && (freqTime < upperBound))
 		{
 			front->sensorBuffer[ipPosition].push_back(freq);
 		}
+		//Place in future frame that can hold it
 		else
 		{
+			bool inFrame = false;
 			for (auto &i : frameBuffer)
 			{
-				if(freqTime >= pointToMilli(i.recievedTime) && (freqTime < pointToMilli(i.recievedTime) + duration))
+				if (freqTime >= pointToMilli(i.recievedTime) && (freqTime < pointToMilli(i.recievedTime) + duration))
 				{
 					i.sensorBuffer[ipPosition].push_back(freq);
+					inFrame = true;
 					break;
 				}
 			}
-			frameBuffer.push_back(FrameBuffer(std::chrono::system_clock::from_time_t(freq.value("time", 0))));
-			//Using end() here causes a dereference error for some reason
-			frameBuffer.back().sensorBuffer.push_back(std::vector<nlohmann::json>());
-			frameBuffer.back().sensorBuffer.resize(ips.size());//This may break existing iterators so watch out for that
+			if (!inFrame)
+			{
+				frameBuffer.push_back(FrameBuffer(std::chrono::system_clock::from_time_t(freq.value("time", 0))));
+				//Using end() here causes a dereference error for some reason
+				frameBuffer.back().sensorBuffer.push_back(std::vector<nlohmann::json>());
+				frameBuffer.back().sensorBuffer.resize(ips.size());//This may break existing iterators so watch out for that
+				frameBuffer.back().sensorBuffer[ipPosition].push_back(freq);
+			}
 		}
 
-		int vFullTracker = 0;
-		if (frameBuffer[cPosition].sensorBuffer[ipPosition].size() >= ips.size())
+		if (globalConfig.type == 0)
 		{
-			for (auto &&i : frameBuffer[cPosition].sensorBuffer)
+			int vFullTracker = 0;
+			if (frameBuffer[cPosition].sensorBuffer[ipPosition].size() >= ips.size())
 			{
-				if (i.size() >= freq.value("size", 0))
+				for (auto &&i : frameBuffer[cPosition].sensorBuffer)
 				{
-					vFullTracker++;
+					if (i.size() >= freq.value("size", 0))
+					{
+						vFullTracker++;
+					}
+				}
+				if (vFullTracker >= ips.size())
+				{
+					transmitBuffer = true;
 				}
 			}
-			if (vFullTracker >= ips.size())
+			if (transmitBuffer)
 			{
-				transmitBuffer = true;
+				frameBuffer.front().Transmit(true, ips, freq, udp);
+				frameBuffer.pop_front();
 			}
-		}
-		if (transmitBuffer)
-		{
-			frameBuffer.front().Transmit(true, ips, freq, udp);
-			frameBuffer.pop_front();
-		}
 
-		std::cout << "Length: " << sData.length << " Sender: " << sData.sender.ip << " Data: " << complete << "\n";
+			std::cout << "Length: " << sData.length << " Sender: " << sData.sender.ip << " Data: " << complete << "\n";
+		}
+		else if (globalConfig.type == 1)
+		{
+
+		}
 	});
 }
 
@@ -161,7 +178,7 @@ void dbSave(uvw::Loop &loop, std::map<std::string, int> &ips)
 
 		sql::PreparedStatement *prep_stmt;
 		prep_stmt = conn->prepareStatement("INSERT INTO sensors(sid, Latitude, Longitude) VALUES (?, ?, ?)");
-		prep_stmt->setInt(1, 12);
+		prep_stmt->setInt(1, 21);
 		prep_stmt->setDouble(2, 2.3210);
 		prep_stmt->setDouble(3, 3.2432);
 		prep_stmt->execute();
